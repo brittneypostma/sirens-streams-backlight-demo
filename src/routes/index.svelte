b30df9364dc8ba25ba1239cc692053b2f3eff762
brittneypostma <brittney.postma@hotmail.com>
2022-02-04 15:06:56 -0500

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Button from '@backlight-dev/backlight-svelte.svelte-kz8llwso/button/src/Button.svelte'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<Button />
